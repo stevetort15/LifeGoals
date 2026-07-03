99619262788fb27dc0d1b39dbdd09abe898cbd71
stevetort15 <stevetort15@gmail.com>
2026-07-02 23:42:00 -0600

    Add files via upload

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,10 @@
+// Componente About: datos de contacto
+// Reto LifeGoals - Steve Tort
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+export class AboutComponent { }
