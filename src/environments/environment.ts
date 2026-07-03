0dbd8253a0e2ae0aac9db33a729b36e5643a2ae1
stevetort15 <stevetort15@gmail.com>
2026-07-02 23:43:50 -0600

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+// Ambiente de producción: keys de conexión al proyecto Firebase LifeGoals
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCRTUBSTlhbS9FMYg1BgfTsryej4LGHHs4",
+    authDomain: "lifegoals-c9a10.firebaseapp.com",
+    projectId: "lifegoals-c9a10",
+    storageBucket: "lifegoals-c9a10.firebasestorage.app",
+    messagingSenderId: "564547418809",
+    appId: "1:564547418809:web:8ae2fa3311b6a3b1af7968"
+  }
+};
