3cd5827962a324760f5952840bc549993f02ca3c
stevetort15 <stevetort15@gmail.com>
2026-07-02 23:40:11 -0600

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+// Punto de entrada: arranca el AppModule (proyecto NgModule, --no-standalone)
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
